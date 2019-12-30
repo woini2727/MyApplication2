31e4f288c6fd36f1a60a3aff92a35efbf0138e15
woini2727 <salvador.woini@gmail.com>
2019-12-30 15:07:16 -0300

    1st commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvToken;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvToken = findViewById(R.id.tvToken);
+
+        tvToken.setText(FirebaseInstanceId.getInstance().getToken());
+    }
+}
